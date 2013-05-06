4900cfde5e1320eeb5cc1efe3d1535183ac1efee
Ville Lautanala <lautis@gmail.com>
2013-05-06 11:02:42 +0300

    Initial commit

---
+++ lib/openid/store/redis.rb
@@ -0,0 +1,9 @@
+require "openid/store/redis/version"
+
+module Openid
+  module Store
+    module Redis
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/openid/store/redis/version.rb
@@ -0,0 +1,7 @@
+module Openid
+  module Store
+    module Redis
+      VERSION = "0.0.1"
+    end
+  end
+end
